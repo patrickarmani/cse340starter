6cc0aa926082077b63c84d8b10a6f3ac17f70f22
patrickarmani <arm22001@byui.edu>
2024-02-13 22:20:05 -0300

    "w03 - 01"

---
+++ database/index.js
@@ -0,0 +1,37 @@
+const { Pool } = require("pg") /* A pool is a collection of connection objects*/
+require("dotenv").config()
+/* ***************
+ * Connection Pool
+ * SSL Object needed for local testing of app
+ * But will cause problems in production environment
+ * If - else will make determination which to use
+ * *************** */
+let pool /*creates a local pool variable to hold the functionality of the "Pool" connection.*/
+if (process.env.NODE_ENV == "development") {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { /* Secure Socket Layer (ssl)*/
+      rejectUnauthorized: false,
+    },
+})
+
+// Added for troubleshooting queries
+// during development
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params)
+      console.log("executed query", { text })
+      return res
+    } catch (error) {
+      console.error("error in query", { text })
+      throw error
+    }
+  },
+}
+} else {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+  })
+  module.exports = pool
+}
--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
+
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -23,9 +25,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 //Index route
-app.get("/", function(req, res){
+/*5 - Alter the route to match the image showed in Alter the "Index Route"*/
+app.get("/", baseController.buildHome)
+/*app.get("/", function(req, res){
   res.render("index", {title: "Home"})
-})
+})*/
 
 /* ***********************
  * Local Server Information
